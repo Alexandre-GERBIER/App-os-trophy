32d8cad081330dc710dd3088314e1c95a61a71aa
Simon ROZEC <simon.rozec1@etu.univ-nantes.fr>
2018-11-09 08:48:58 +0100

    Initialisation du projet

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1 is="sui-header">{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      name: 'App\'Os Trophy'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
