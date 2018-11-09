6d5bf7e7cd7d473d00ad2495ca6b3657528ee6e9
Thomas LEGER <thomas.leger1@etu.univ-nantes.fr>
2018-11-09 14:47:18 +0100

    Création de la page de connexion + ajout d'images

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,21 @@
 <template>
-    <div>
-        <h2 is="sui-header">Login</h2>
+<div id="background">
+    <form id="login">
+        <h1 is="sui-header" class="inverted">App'Os Trophy</h1>
+        <h2 is="sui-header" class="inverted">Connexion</h2>
+        <div class="ui user left icon input">
+            <input placeholder="Identifiant" type="text">
+            <i class="user icon"></i>
+        </div>
+        <div class="ui lock left icon input">
+            <input placeholder="Mot de passe" type="password">
+            <i class="lock icon"></i>
+        </div>
         <router-link to="/student/profile">
-            <sui-button>Login</sui-button>
+            <button class="ui icon sign in right labeled button green"><i class="sign in icon"></i>Login</button>
         </router-link>
-    </div>
+    </form>
+</div>
 </template>
 
 <script>
@@ -13,6 +24,33 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    #background{
+        background-image:url("/static/images/2.jpg");
+        background-size:cover;
+        background-position: center;
+        position: absolute;
+        height:100%;
+        width:100%;
+    }
+    #login{
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        position:relative;
+        top:45%;
+    }
+    #login h2, #login div{
+        margin: 10px;
+    }
+    #login h2{
+        font-size: 2.5em;
+    }
+    #login h1{
+        margin-bottom:40px;
+        font-size: 4.5em;
+    }
+    input{
+        width:500px;
+    }
 </style>
