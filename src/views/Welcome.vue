3a2a088611f57f3d5f7cb647a08f8926b35eff20
Alexandre GERBIER <alexandre.gerbier@etu.univ-nantes.fr>
2018-11-12 09:55:28 +0100

    ajout de la vue welcome

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,9 +1,24 @@
 <template>
-    <div>
-        <h2 is="sui-header">Welcome</h2>
-        <router-link to="login">
-            <sui-button>Login</sui-button>
-        </router-link>
+    <div id="main">
+        <div id="parent">
+        <h2 id="center"> App'Os Trophy </h2>
+        <sui-grid class=centered>
+            <sui-grid-row>
+                <sui-grid-column :width="6">
+                    <p>Le site App'OS trophy est un outil mis à la disposition des enseignants pour ludifier l'apprentissage de leur matière.
+                    Le site permet de distribuer des trophées aux élèves pour stimuler leur compétitivité et favoriser l'émulation intellectuelle.
+                    Les élèves peuvent voter entre eux pour l'attribution de trophées.
+                    </p>
+                </sui-grid-column>
+            </sui-grid-row>
+        </sui-grid>
+        <sui-divider hidden></sui-divider>
+        <div id="center">
+            <router-link to="login">
+                <sui-button positive>Identification</sui-button>
+            </router-link>
+        </div>
+        </div>
     </div>
 </template>
 
@@ -13,6 +28,25 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+p {
+    text-align: justify;
+}
+#center {
+    text-align: center;
+}
+#main {
+    background-image: url("/static/images/Welcome-back.jpg");
+    background-size: cover;
+    background-position: center;
+    height: 100%;
+    position: absolute;
+}
+#parent {
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    position:relative;
+    top: 30%;
+}
 </style>
