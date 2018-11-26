bb15b4f0c0829fc24747672f6efaca3bf54a4e83
Nathan Genaudeau <nathan.genaudeau@etu.univ-nantes.fr>
2018-11-26 17:59:02 +0100

    Modification vue Trophy

--- src/views/student/Trophy.vue
+++ src/views/student/Trophy.vue
@@ -1,6 +1,7 @@
     <template>
     <div>
-        <h3 is="sui-header">View Trophy and Vote {{ $route.params.id}} {{nom}}</h3>
+        <h3 is="sui-header">View Trophy and Vote {{ $route.params.id}} appel a l'api</h3>
+        <p>INFOS</p>
     </div>
 </template>
 
@@ -10,37 +11,7 @@
 export default {
 
   data () {
-    return {
-      trophies: [
-        {
-          id: '1',
-          nom: 'Toujours présent',
-          module: 'maths',
-          date: '21/05/2018',
-          valeur: 'bronze',
-          vote: false,
-          obtenu: true
-        },
-        {
-          id: '2',
-          nom: 'Toujours prêt',
-          module: 'algo',
-          date: '30/10/2018',
-          valeur: 'or',
-          vote: true,
-          obtenu: false
-        },
-        {
-          id: '3',
-          nom: 'Toujours debout',
-          module: 'bdd',
-          date: '08/02/2018',
-          valeur: 'or',
-          vote: true,
-          obtenu: true
-        }
-      ]
-    }
+
   }
 }
 </script>
