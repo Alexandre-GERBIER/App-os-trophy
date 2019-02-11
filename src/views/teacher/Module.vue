f366a1814ee52891813ddeff68d9d07784889fc4
Nathan Genaudeau <nathan.genaudeau@etu.univ-nantes.fr>
2019-02-11 10:26:47 +0100

    modification ergonomie boutons dans différentes vues

--- src/views/teacher/Module.vue
+++ src/views/teacher/Module.vue
@@ -2,38 +2,39 @@
     <div>
         <h2 is="sui-header">Module {{ $route.params.reference }} - {{name}} </h2>
         <sui-grid class="centered">
-                <sui-grid-column :width="8">
-                    <sui-divider fitted/>
-                    <p>Nombre d'étudiants inscrits au module : <b>{{numetu}}</b></p>
-                    <sui-divider hidden/>
-                    <sui-divider hidden/>
+            <sui-grid-column :width="8">
+                <sui-divider fitted/>
+                <p>Nombre d'étudiants inscrits au module : <b>{{numetu}}</b></p>
+                <sui-divider hidden/>
+                <sui-divider hidden/>
 
-                    <h3 is="sui-header">Trophées ( {{numtro}} disponibles )</h3>
-                    <sui-container>
-                    <sui-table unstackable>
-                      <sui-table-header>
-                          <sui-table-row>
-                              <sui-table-header-cell>Nom</sui-table-header-cell>
-                              <sui-table-header-cell>Valeur</sui-table-header-cell>
-                              <sui-table-header-cell>Vote</sui-table-header-cell>
-                              <sui-table-header-cell>Délivrer</sui-table-header-cell>
-                              <sui-table-header-cell text-align="right">Informations</sui-table-header-cell>
-                          </sui-table-row>
-                      </sui-table-header>
-                      <sui-table-body>
-                          <sui-table-row v-for="trophy in trophies" :key="trophy.nom">
-                              <sui-table-cell>{{trophy.titre}}</sui-table-cell>
-                              <sui-table-cell><img width=40 height=40 :src="'/static/images/imageTrophee' + trophy.type.charAt(0).toUpperCase() + trophy.type.slice(1) + '.png'" /></sui-table-cell>
-                              <sui-table-cell ><i class="check icon" v-if="!trophy.vote" ></i></sui-table-cell>
-                              <sui-table-cell ><DeliverTrophy/></sui-table-cell>
-                              <sui-table-cell text-align="right"><EditTrophy/></sui-table-cell>
-                          </sui-table-row>
-                      </sui-table-body>
-                    </sui-table>
-                  </sui-container>
-                </sui-grid-column>
+                <h3 is="sui-header">Trophées ( {{numtro}} disponibles )</h3>
+                <sui-container>
+                <sui-table unstackable>
+                  <sui-table-header>
+                      <sui-table-row>
+                          <sui-table-header-cell>Nom</sui-table-header-cell>
+                          <sui-table-header-cell>Valeur</sui-table-header-cell>
+                          <sui-table-header-cell>Vote</sui-table-header-cell>
+                          <sui-table-header-cell>Délivrer</sui-table-header-cell>
+                          <sui-table-header-cell text-align="right">Informations</sui-table-header-cell>
+                      </sui-table-row>
+                  </sui-table-header>
+                  <sui-table-body>
+                      <sui-table-row v-for="trophy in trophies" :key="trophy.nom">
+                          <sui-table-cell>{{trophy.titre}}</sui-table-cell>
+                          <sui-table-cell><img width=40 height=40 :src="'/static/images/imageTrophee' + trophy.type.charAt(0).toUpperCase() + trophy.type.slice(1) + '.png'" /></sui-table-cell>
+                          <sui-table-cell ><i class="check icon" v-if="trophy.vote == 1" ></i></sui-table-cell>
+                          <sui-table-cell ><DeliverTrophy/></sui-table-cell>
+                          <sui-table-cell text-align="right"><EditTrophy/></sui-table-cell>
+                      </sui-table-row>
+                  </sui-table-body>
+                </sui-table>
+              </sui-container>
+              <br/>
+              <CreateTrophy/>
+            </sui-grid-column>
         </sui-grid>
-      <CreateTrophy/>
     </div>
 </template>
 
@@ -60,15 +61,13 @@ export default {
     }
   },
   mounted () {
-    console.log('oui')
-    axios.get(global.API + '/module/' + this.$route.params.reference)
+    // let promesses = []
+    /* promesses.push( */ axios.get(global.API + '/module/' + this.$route.params.reference)
       .then(res => {
         this.name = res.data[0].nom
-        console.log('1 : ' + this.name)
         axios.get(global.API + '/module/' + this.$route.params.reference)
           .then(res => {
             this.numetu = res.data.length
-            console.log('2 : ' + this.numetu)
           })
           .catch(e => {
             this.errors.push(e)
@@ -77,7 +76,6 @@ export default {
           .then(res => {
             this.numtro = res.data.length
             this.trophies = res.data
-            console.log('3 : ' + this.numtro)
           })
           .catch(e => {
             this.errors.push(e)
@@ -85,7 +83,10 @@ export default {
       })
       .catch(e => {
         this.errors.push(e)
-      })
+      }) // )
+    /* Promise.all(promesses).then(function () {
+      console.log('fini')
+    }) */
   }
 }
 </script>
