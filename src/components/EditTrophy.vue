b3da5c1c7f2fe8d0cc09fc751fb4118f54a9859a
Alexandre Gerbier <alexandre.gerbier@etu.univ-nantes.fr>
2019-02-12 10:06:02 +0100

    responsive vue éditer trophée

--- src/components/EditTrophy.vue
+++ src/components/EditTrophy.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="boutonTrophy">
     <sui-button @click.native="toggle">Éditer un trophée</sui-button>
-    <sui-modal v-model="open">
+    <sui-modal v-model="open" id="big_popup">
       <sui-modal-header >éditer un trophée : {{ trophy[0].titre }} </sui-modal-header>
       <form class="ui grid">
         <div class="four wide column">
@@ -56,6 +56,86 @@
         <sui-button negative @click.native="toggle">Annuler</sui-button>
       </sui-modal-actions>
     </sui-modal>
+    <sui-modal v-model="open" id="small_popup">
+      <sui-modal-header >éditer un trophée : {{ trophy[0].titre }} </sui-modal-header>
+      <form >
+        <sui-grid>
+          <sui-grid-row>
+            <sui-grid-column :width="5" >
+              <sui-image v-if="trophy[0].type === 'platine'" label="imageTrophy" src="/static/images/imageTropheePlatine.png" size="tiny" />
+              <sui-image v-else-if="trophy[0].type === 'or'" label="imageTrophy" src="/static/images/imageTropheeOr.png" size="tiny" />
+              <sui-image v-else-if="trophy[0].type === 'argent'" label="imageTrophy" src="/static/images/imageTropheeArgent.png" size="tiny" />
+              <sui-image v-else-if="trophy[0].type === 'bronze'" label="imageTrophy" src="/static/images/imageTropheeBronze.png" size="tiny" />
+           </sui-grid-column>
+            <sui-grid-column :width="5" >
+              <label> Titre :  </label>
+              {{ trophy[0].titre }}
+            </sui-grid-column>
+            <sui-grid-column :width="5" >
+              Module : {{ trophy[0].numod}}
+            </sui-grid-column>
+          </sui-grid-row>
+          <sui-grid-row >
+            <sui-grid-column :width="six"> Valeur du trophée : </sui-grid-column>
+          </sui-grid-row >
+          <sui-grid-row >
+            <sui-grid-column :width="3"></sui-grid-column>
+            <sui-grid-column :width="5">
+              <sui-form-field>
+                <sui-checkbox name="TrophyValue" label="platine" radio value="platine" v-model="TrophyValue"/>
+              </sui-form-field>
+            </sui-grid-column>
+            <sui-grid-column :width="5" >
+              <sui-form-field>
+                <sui-checkbox name="TrophyValue" label="or" radio value="or" v-model="TrophyValue"/>
+              </sui-form-field>
+            </sui-grid-column>
+            <sui-grid-column :width="15"></sui-grid-column>
+            <sui-grid-column :width="3"></sui-grid-column>
+            <sui-grid-column :width="5" >
+              <sui-form-field>
+                <sui-checkbox name="TrophyValue" label="argent" radio value="argent" v-model="TrophyValue"/>
+              </sui-form-field>
+            </sui-grid-column>
+            <sui-grid-column :width="5" >
+              <sui-form-field>
+                <sui-checkbox name="TrophyValue" label="bronze" radio value="bronze" v-model="TrophyValue"/>
+              </sui-form-field>
+            </sui-grid-column>
+          </sui-grid-row>
+          <sui-grid-row>
+            <sui-grid-column class="six wide column">
+              <label> Conditions d'obtentions: </label>
+              <sui-input name="descTrophy" :value="trophy[0].description" size="25"/>
+            </sui-grid-column>
+          </sui-grid-row>
+          <sui-grid-row >
+            <sui-grid-column >
+              <sui-checkbox label="Visible" toggle v-model="visible"/>
+            </sui-grid-column>
+          </sui-grid-row>
+          <sui-grid-row>
+            <sui-grid-column>
+              <datepicker class="ui input" v-show="visible" :monday-first="true" :language="fr" :full-month-name="true"></datepicker>
+            </sui-grid-column>
+          </sui-grid-row>
+          <sui-grid-row>
+            <sui-grid-column>
+              <sui-checkbox label="vote" toggle v-model="vote"/>
+            </sui-grid-column>
+          </sui-grid-row>
+          <sui-grid-row>
+            <sui-grid-column>
+              <datepicker v-show="vote" :monday-first="true" :language="fr" :full-month-name="true"></datepicker>
+            </sui-grid-column>
+          </sui-grid-row>
+        </sui-grid>
+      </form>
+      <sui-modal-actions>
+        <sui-button positive>Éditer</sui-button>
+        <sui-button negative @click.native="toggle">Annuler</sui-button>
+      </sui-modal-actions>
+    </sui-modal>
   </div>
 </template>
 
@@ -64,10 +144,13 @@ import Datepicker from 'vuejs-datepicker'
 import axios from 'axios'
 import {fr} from 'vuejs-datepicker/dist/locale'
 import global from '@/globals.json'
+import SuiGrid from 'semantic-ui-vue/dist/commonjs/collections/Grid/Grid'
+import SuiGridRow from 'semantic-ui-vue/dist/commonjs/collections/Grid/GridRow'
+import SuiGridColumn from 'semantic-ui-vue/dist/commonjs/collections/Grid/GridColumn'
 
 export default {
   name: 'EditTrophy',
-  components: { Datepicker },
+  components: {SuiGridColumn, SuiGridRow, SuiGrid, Datepicker },
   props: {
     id: ''
   },
@@ -100,7 +183,17 @@ export default {
 </script>
 
 <style>
+  @media (max-width: 700px) {
+    #big_popup {
+      display: none;
+    }
+  }
 
+  @media (min-width: 701px) {
+    #small_popup {
+      display: none;
+    }
+  }
   #boutonPassword{
     text-align: center;
   }
