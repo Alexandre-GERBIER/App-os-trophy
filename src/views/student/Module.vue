08ac2853c610f673802dec4582413c0e4ee1a8fa
Thomas Léger <thomas.leger1@etu.univ-nantes.fr>
2019-02-12 15:40:45 +0100

    bon nom pour vote dans module

--- src/views/student/Module.vue
+++ src/views/student/Module.vue
@@ -24,7 +24,7 @@
                               <sui-table-cell><router-link :to="'/student/trophy/' + trophy.nutroph">{{trophy.titre}}</router-link></sui-table-cell>
                               <sui-table-cell><img width=40 height=40 :src="'/static/images/imageTrophee' + trophy.type.charAt(0).toUpperCase() + trophy.type.slice(1) + '.png'" /></sui-table-cell>
                               <sui-table-cell>
-                                <vote class="check icon" :id="trophy.nutroph"  :name="trophy.titre" v-if="trophy.vote == 1"/>
+                                <vote class="check icon" :id="trophy.nutroph"  :name="trophy.titre" :numod="trophy.titre" v-if="trophy.vote == 1"/>
                                 <i v-else class="close icon"></i>
                               </sui-table-cell>
                           </sui-table-row>
